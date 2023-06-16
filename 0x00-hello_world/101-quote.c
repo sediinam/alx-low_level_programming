eac2f36b9892deb1fa3f1f7de8731857f975fabd
sedinam <sedgraham@icloud.com>
2023-06-15 19:48:32 -0700

    Hellow World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,9 @@
 #include <stdio.h>
 #include <unistd.h>
-<<<<<<< HEAD
-/**
- * main - Entry point
- * Return:1
- */
-=======
-
 /**
 * main - Entry point
 * Return:1
 */
->>>>>>> 1ff15639b3dd2707d91b4128636c22af0a024fa8
 int main(void)
 {
 fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
